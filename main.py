dab229acc7113b0b6ef2190b4c10f9b8327c668a
Jon <jonmhenrich@gmail.com>
2025-01-13 13:33:16 -0500

    Added delta time and delay.

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print(f"Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -19,6 +21,7 @@ def main():
         
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
 
 
 if __name__ == "__main__":
